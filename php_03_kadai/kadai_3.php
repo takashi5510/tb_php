131015c3f53c5f4ba87014469a2084334028c6a0
Takashi Ishii <takashi5510@gmail.com>
2018-09-12 18:36:43 +0900

    php_03_kadai add

---
+++ php_03_kadai/kadai_3.php
@@ -0,0 +1,6 @@
+<?php
+$hello = "Hello,";
+$name = "Takashi Ishii";
+$world = "'s World!";
+echo $hello, $name, $world, "\n";
+?>
---
+++ php_03_kadai/kadai_5.php
@@ -0,0 +1,26 @@
+<?php
+//$2018_calendar = {
+//$calendar = {
+$calendar = [
+  "January" => "1月",
+//  "February" => "2月"、
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+//  "June" = "6月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+//  "October" => 10月,
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+//};
+];
+
+// 12月を表示する
+//echo $2018_calendar[December];
+echo $calendar[December], "\n";
+?>
